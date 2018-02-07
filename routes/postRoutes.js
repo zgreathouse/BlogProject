6a19a2f065a937206bebf2e89935ed72a0cb4e11
zgreathouse <greathouse.zachary@gmail.com>
2018-02-07 12:37:47 -0800

    build post routes for: fetching all the posts, fetching one post, creating a post, and deleting a post. routes not yet tested.

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,1 +1,62 @@
 const mongoose = require('mongoose');
+const Path = require('path-parser');
+const { URL } = require('url');
+
+const Post = mongoose.model('posts');
+
+module.exports = app => {
+  // Get an index of all the the posts
+  app.get('/api/posts', async (req, res) => {
+    try {
+      const allPosts = await Post.find({});
+      res.send(allPosts);
+    } catch (err) {
+      res.status(422).send(err);
+    }
+  });
+
+  // Get a single post
+  app.get('/api/posts/:postID', async (req, res) => {
+    const p = new Path('/api/posts/:postID');
+    const match = p.test(req.url);
+
+    const post = await Post.findOne({ _id: match.postID });
+
+    res.send(post);
+  });
+
+  //create a new post in the database
+  app.post('/api/posts', async (req, res) => {
+     const post = new Post({
+       title: req.body.title,
+       body: req.body.body,
+       imageURL: req.body.imageURL,
+       videoURL: req.body.videoURL,
+       datePosted: req.body.datePosted,
+       comments: []
+     });
+
+     try {
+       await post.save();
+       res.send("Post successfully created!");
+     } catch (err) {
+       res.status(422).send(err);
+     }
+  });
+
+  // Delete an existing post
+  app.delete('/api/posts/:postID', async (req, res) => {
+    const p = new Path('/api/posts/:postID')
+    const match = p.test(req.url);
+
+    const post = await Post.findOne({ _id: match.postID })
+
+    try {
+      //removes tournament
+      Post.findByIdAndRemove(post.id).exec();
+      res.send("Post successfully deleted");
+    } catch (err) {
+      res.status(422).send(err);
+    }
+  });
+}
