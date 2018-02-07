58315973daaaf7766697586eb0c2dbccd981d0cb
zgreathouse <greathouse.zachary@gmail.com>
2018-02-07 12:23:55 -0800

    add postRoutes and commentRoutes files to routes folder, create an instance of express called app in server index file

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const mongoose = require('mongoose');
 require('./models/Post');
 require('./models/Comment');
 
+//create instance of express
+const app = express();
+
+
+
 //dynamic port binding
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
